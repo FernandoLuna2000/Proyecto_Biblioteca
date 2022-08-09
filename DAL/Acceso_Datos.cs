0d4d5031d734d77d3ca2f8bf5dbc2c76ce391f5c
FernandoLuna2000 <utp0138624@alumno.utpuebla.edu.mx>
2022-08-09 17:05:22 -0500

    Conexion Base de Datos

--- DAL/Acceso_Datos.cs
+++ DAL/Acceso_Datos.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,59 @@ namespace DAL
 {
     public class Acceso_Datos
     {
+        private string Conexion;
+        public Acceso_Datos(string connection)
+        {
+            Conexion = connection;
+        }
+        public SqlConnection ConnectionEstablecida(ref string mensajeC)
+        {
+            SqlConnection puerto = new SqlConnection();
+            puerto.ConnectionString = Conexion;
+            try
+            {
+                puerto.Open();
+                mensajeC = "Connexion Establecida";
+            }
+            catch (Exception e)
+            {
+                mensajeC = "Error: " + e;
+                puerto = null;
+            }
+            return puerto;
+        }
+        public DataSet LecturaSet(string comandoSql, SqlConnection conAbierta, ref string mensaje, string etiqueta)
+        {
+            SqlCommand comando = null;
+            DataSet dataSet = null;
+            SqlDataAdapter dataAdapter = null;
+
+            if (conAbierta == null)
+            {
+                dataSet = null;
+            }
+            else
+            {
+                using (comando = new SqlCommand(comandoSql, conAbierta))
+                {
+                    using (dataAdapter = new SqlDataAdapter())
+                    {
+                        dataSet = new DataSet();
+                        dataAdapter.SelectCommand = comando;
+                        try
+                        {
+                            dataAdapter.Fill(dataSet, etiqueta);
+                            mensaje = "Recuperacion Correcta";
+                        }
+                        catch (Exception e)
+                        {
+                            mensaje = "Lo siento: " + e.Message;
+                            dataSet = null;
+                        }
+                    }
+                }
+            }
+            return dataSet;
+        }
     }
 }
